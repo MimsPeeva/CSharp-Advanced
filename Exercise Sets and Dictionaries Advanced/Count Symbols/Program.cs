32f8d1f81615884aa9a36cc78a265fb68fd31b09
MimsPeeva <mpeeva04@abv.bg>
2024-01-21 17:54:41 +0200

    Add new projects

---
+++ Exercise Sets and Dictionaries Advanced/Count Symbols/Program.cs
@@ -0,0 +1,18 @@
+﻿char[] text = Console.ReadLine()
+  .ToCharArray();
+SortedDictionary<char, int> dictionary = new SortedDictionary<char, int>();
+foreach(char ch in text)
+{ 
+    if (dictionary.ContainsKey(ch))
+    {
+        dictionary[ch]++;
+    }
+    else
+    {
+        dictionary.Add(ch,1); 
+    }
+}
+foreach (char ch in dictionary.Keys)
+{
+    Console.WriteLine($"{ch}: {dictionary[ch]} time/s");
+}
