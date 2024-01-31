59d1ebc08ef151541f2ba5134988099b2b4f7138
MimsPeeva <mpeeva04@abv.bg>
2024-01-31 21:49:06 +0200

    Add new projects

--- Exercise Functional Programming/Party Reservation Filter Module/Program.cs
+++ Exercise Functional Programming/Party Reservation Filter Module/Program.cs
@@ -2,75 +2,46 @@
     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
     .ToList();
 string lines = string.Empty;
+List<string> removedAfterFiltering = new List<string>();
 while ((lines = Console.ReadLine()) != "Print")
 {
     string[] tokens = lines.Split(";", StringSplitOptions.RemoveEmptyEntries);
     string command = tokens[0];
     string filterType = tokens[1];
     string filterParameter = tokens[2];
-    Func<string, bool> predicate = GetPredicate(filterType,filterParameter);
-    if (command == "Add filter")
-    {
-        commingPeople = AddFilter(commingPeople,predicate);
-    }
-    else if (command == "Remove filter")
-    {
-        commingPeople = RemoveFilter(commingPeople, predicate);
+  
 
+    Predicate<string> filter = GetPredicate(filterType, filterParameter);
+
+    switch (command)
+    {
+        case "Add filter":
+            removedAfterFiltering.AddRange(commingPeople.Where(x => filter(x)));
+            commingPeople.RemoveAll(filter);
+            break;
+        case "Remove filter":
+            commingPeople.AddRange(commingPeople.Where(x => filter(x)));
+            break;
     }
 }
-Console.WriteLine(commingPeople.Any() ?
-    $"{string.Join(", ", commingPeople)}" :
-    "");
+Console.WriteLine(string.Join(' ', commingPeople));
 
 
-List<string> RemoveFilter(List<string> guestList, Func<string, bool> predicate)
+static Predicate<string> GetPredicate(string filterType, string filterParameter)
 {
-    List<string> result = new List<string>();
-    foreach (string guest in guestList)
+    switch (filterType)
     {
-        if (result.Contains(guest))
-        {
-            result.Remove(guest);
-        }
+        case "Starts with":
+            return g => g.StartsWith(filterParameter);
+        case "Ends with":
+            return g => g.EndsWith(filterParameter);
+        case "Length":
+            return g => g.Length == int.Parse(filterParameter);
+        case "Contains":
+            return g => g.Contains(filterParameter);
     }
-    return result;
-}
 
-List<string> AddFilter(List<string> guestList, Func<string, bool> predicate)
-{
-    List<string> result = new List<string>();
-    foreach (string guest in guestList)
-    {
-        if (!predicate(guest))
-        {
-            result.Add(guest);
-        }
-    }
-    return result;
-}
-
-Func<string, bool> GetPredicate(string filterType, string filterParameter)
-{
-    if (filterType == "Starts with")
-    {
-        return n => n.StartsWith(filterParameter);
-    }
-    if (filterType == "Ends with")
-    {
-        return n => n.EndsWith(filterParameter);
-
-    }
-    if (filterType == "Length")
-    {
-        return n => n.Length==int.Parse(filterParameter);
-    }
-    if (filterType == "Contains")
-    {
-        return n => n.Contains(filterParameter);
-
-    }
-    return default;
+    return x => true;
 
 }
 /*
---
+++ Exercise Functional Programming/TriFunction/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+int number = int.Parse(Console.ReadLine());
+string[] names = Console.ReadLine()
+    .Split(' ',StringSplitOptions.RemoveEmptyEntries);
+Func<string, int, bool> getSum = (name, number) => name.Sum(x=>x) >= number;
+Func <string[], int, Func<string, int, bool>,string> getFirstName = (name, min, check) => names.First(x=>check(x,min));
+Console.WriteLine(getFirstName(names,number,getSum));
