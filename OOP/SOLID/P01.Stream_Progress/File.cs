5716ec0b3426b9e77d1e3cc9da8ff22d60db0133
MimsPeeva <mpeeva04@abv.bg>
2024-03-11 21:20:07 +0200

    Add new projects

---
+++ OOP/SOLID/P01.Stream_Progress/File.cs
@@ -0,0 +1,16 @@
+﻿namespace P01.Stream_Progress
+{
+    public class File:IStreamable
+    {
+        private string name;
+
+        public File(string name, int length, int bytesSent):
+             base(length, bytesSent)
+        {
+            this.name = name;
+            
+        }
+
+     
+    }
+}
---
+++ OOP/SOLID/P03.Detail_Printer/DetailsPrinter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.DetailPrinter
+{
+    public class DetailsPrinter
+    {
+        private IList<Employee> employees;
+
+        public DetailsPrinter(IList<Employee> employees)
+        {
+            this.employees = employees;
+        }
+
+        public void PrintDetails(Employee employee)
+        {
+            Print(employee);
+        }
+        public void Print(Employee employee)
+        {
+            employee.Print();
+
+        }
+
+
+
+
+    }
+}
